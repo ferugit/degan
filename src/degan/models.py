d24b49da3190473d5ce63053c549c2e52650ad9a
Fer <william.lopez.practicas@telefonica.com>
2021-05-11 22:31:47 +0200

    add GAN models

--- src/degan/models.py
+++ src/degan/models.py
@@ -92,133 +92,187 @@ class LeNet(SerializableModule):
         return int(math.floor((((in_dim-4)/2)-4)/2))
 
 
-@SerializableModule.register_model('crnn')
-class CRNN(SerializableModule):
-    """
-    Model based on Kiran's Sanjeevan model: https://github.com/ksanjeevan/crnn-audio-classification
-    """
-    def __init__(self, num_classes):
+class Transpose1dLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding=11, upsample=None, output_padding=1):
         super().__init__()
+        self.upsample = upsample
 
-        # Mel-Spectrogram
-        self.mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-            sample_rate=16000,
-            n_fft=512,
-            win_length=320,
-            hop_length=160,
-            n_mels=40
-        )
-
-        # Feature Extraction: CNN
-        self.features = nn.Sequential(
-            # Conv 1
-            nn.Conv2d(1, 32, kernel_size=3),
-            nn.BatchNorm2d(32),
-            nn.ELU(),
-            nn.MaxPool2d(3),
-            # Conv 2
-            nn.Conv2d(32, 64, kernel_size=3),
-            nn.BatchNorm2d(64),
-            nn.ELU(),
-            nn.MaxPool2d(2),
-            # Conv 3
-            nn.Conv2d(64, 64, kernel_size=3),
-            nn.BatchNorm2d(64),
-            nn.ELU(),
-            nn.MaxPool2d(2),
-            nn.Dropout(0.1)
-        )
-
-        # RNN
-        self.lstm = nn.LSTM(input_size=64, hidden_size=64, num_layers=2)
-
-        # Classifier
-        self.classifier = nn.Sequential(
-            nn.Linear(640, 100),
-            nn.Dropout(0.3),
-            nn.Linear(100, num_classes),
-
-        )
+        self.upsample_layer = torch.nn.Upsample(scale_factor=upsample)
+        reflection_pad = kernel_size // 2
+        self.reflection_pad = nn.ConstantPad1d(reflection_pad, value=0)
+        self.conv1d = torch.nn.Conv1d(in_channels, out_channels, kernel_size, stride)
+        self.Conv1dTrans = nn.ConvTranspose1d(in_channels, out_channels, kernel_size, stride, padding, output_padding)
 
     def forward(self, x):
-        x = x.unsqueeze(1) # [b, ch, t]
-        x = self.mel_spectrogram(x) # [b, ch, f_b, t_b]
-        x = self.features(x)
-        x = x.permute(0, 3, 1, 2) # [b, t_b, ch, f_b]
-        x = x.view(x.shape[0], x.shape[1], -1) # [b, t_b, ch*f_b]
-        x, _ = self.lstm(x) # [b, t_b, 64]
-        x = x.view(x.shape[0], -1) # [b, t_b*64]
-        x = self.classifier(x) # [b, 10]
-        return x
-
-
-class SpeechResnetX(SerializableModule):
-    """
-    ResNetX architecture for Audio Classification.
-    """
-    def __init__(self, num_classes, n_layers, n_maps, res_pool=None, dilation=True):
-        super().__init__()
-
-        self.n_layers = n_layers
-
-        # Fisrt convolution
-        self.conv0 = nn.Conv2d(1, n_maps, (3, 3), padding=(1, 1), bias=False)
-        if(res_pool):
-            self.pool = nn.AvgPool2d(res_pool)
-
-        # Create convolutional layers
-        if(dilation):
-            self.convs = [nn.Conv2d(n_maps, n_maps, (3, 3), padding=int(2**(i // 3)), dilation=int(2**(i // 3)),
-                bias=False) for i in range(n_layers)]
+        if self.upsample:
+            return self.conv1d(self.reflection_pad(self.upsample_layer(x)))
         else:
-            self.convs = [nn.Conv2d(n_maps, n_maps, (3, 3), padding=1, dilation=1,
-                bias=False) for _ in range(n_layers)]
-        
-        # Add Convolutional layers with BatchNormalization
-        for i, conv in enumerate(self.convs):
-            self.add_module("bn{}".format(i + 1), nn.BatchNorm2d(n_maps, affine=False))
-            self.add_module("conv{}".format(i + 1), conv)
-        
-        # Classifier
-        self.fc = nn.Linear(n_maps, num_classes)
+            return self.Conv1dTrans(x)
 
-    def forward(self, x):
-        x = x.unsqueeze(1) # add channel dimension
-        for i in range(self.n_layers + 1):
-            y = nn.functional.relu(getattr(self, "conv{}".format(i))(x))
-            # If average pool after first layer
-            if i == 0:
-                if hasattr(self, "pool"):
-                    y = self.pool(y)
-                old_x = y
-            if i > 0 and i % 2 == 0:
-                x = y + old_x
-                old_x = x
-            else:
-                x = y
-            if i > 0:
-                x = getattr(self, "bn{}".format(i))(x)
-        x = x.view(x.size(0), x.size(1), -1) # shape: (batch, feats, o3)
-        x = torch.mean(x, 2)
-        return self.fc(x)
+@SerializableModule.register_model('generator')
+class Generator(SerializableModule):
 
-
-@SerializableModule.register_model('resnet15')
-class Resnet15(SerializableModule):
-    """
-    ResNet15 architecture for WuW detection.
-    """
-    def __init__(self, num_classes=10):
+    def __init__(self, model_size=64, num_channels=1,
+                 latent_dim=100, post_proc_filt_len=512,
+                 verbose=False, upsample=True):
         super().__init__()
-        self.mel_spectrogram = torchaudio.transforms.MelSpectrogram(
-            sample_rate=16000,
-            n_fft=512,
-            win_length=320,
-            hop_length=160,
-            n_mels=40
-        )
-        self.model = SpeechResnetX(num_classes, n_layers=13, n_maps=45, res_pool=None, dilation=True)
+
+        self.model_size = model_size  # d
+        self.num_channels = num_channels  # c
+        self.latent_di = latent_dim
+        self.post_proc_filt_len = post_proc_filt_len
+        self.verbose = verbose
+        self.fc1 = nn.Linear(latent_dim, 256 * model_size)
+
+        stride = 4
+        if upsample:
+            stride = 1
+            upsample = 4
+        self.deconv_1 = Transpose1dLayer(16 * model_size, 8 * model_size, 25, stride, upsample=upsample)
+        self.deconv_2 = Transpose1dLayer(8 * model_size, 4 * model_size, 25, stride, upsample=upsample)
+        self.deconv_3 = Transpose1dLayer(4 * model_size, 2 * model_size, 25, stride, upsample=upsample)
+        self.deconv_4 = Transpose1dLayer(2 * model_size, model_size, 25, stride, upsample=upsample)
+        self.deconv_5 = Transpose1dLayer(model_size, num_channels, 25, stride, upsample=upsample)
+
+        if post_proc_filt_len:
+            self.ppfilter1 = nn.Conv1d(num_channels, num_channels, post_proc_filt_len)
+
+        for m in self.modules():
+            if isinstance(m, nn.ConvTranspose1d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal(m.weight.data)
 
     def forward(self, x):
-        x = self.mel_spectrogram(x)
-        return self.model(x)
+        x = self.fc1(x).view(-1, 16 * self.model_size, 16)
+        x = torch.nn.functional.relu(x)
+        if self.verbose:
+            print(x.shape)
+
+        x = torch.nn.functional.relu(self.deconv_1(x))
+        if self.verbose:
+            print(x.shape)
+
+        x = torch.nn.functional.relu(self.deconv_2(x))
+        if self.verbose:
+            print(x.shape)
+
+        x = torch.nn.functional.relu(self.deconv_3(x))
+        if self.verbose:
+            print(x.shape)
+
+        x = torch.nn.functional.relu(self.deconv_4(x))
+        if self.verbose:
+            print(x.shape)
+
+        output = torch.nn.functional.tanh(self.deconv_5(x))
+        return output
+
+
+class PhaseShuffle(nn.Module):
+    """
+    Performs phase shuffling, i.e. shifting feature axis of a 3D tensor
+    by a random integer in {-n, n} and performing reflection padding where
+    necessary.
+    """
+    # Copied from https://github.com/jtcramer/wavegan/blob/master/wavegan.py#L8
+    def __init__(self, shift_factor):
+        super(PhaseShuffle, self).__init__()
+        self.shift_factor = shift_factor
+
+    def forward(self, x):
+        if self.shift_factor == 0:
+            return x
+        # uniform in (L, R)
+        k_list = torch.Tensor(x.shape[0]).random_(0, 2 * self.shift_factor + 1) - self.shift_factor
+        k_list = k_list.numpy().astype(int)
+
+        # Combine sample indices into lists so that less shuffle operations
+        # need to be performed
+        k_map = {}
+        for idx, k in enumerate(k_list):
+            k = int(k)
+            if k not in k_map:
+                k_map[k] = []
+            k_map[k].append(idx)
+
+        # Make a copy of x for our output
+        x_shuffle = x.clone()
+
+        # Apply shuffle to each sample
+        for k, idxs in k_map.items():
+            if k > 0:
+                x_shuffle[idxs] = torch.nn.functional.pad(x[idxs][..., :-k], (k, 0), mode='reflect')
+            else:
+                x_shuffle[idxs] = torch.nn.functional.pad(x[idxs][..., -k:], (0, -k), mode='reflect')
+
+        assert x_shuffle.shape == x.shape, "{}, {}".format(x_shuffle.shape,
+                                                       x.shape)
+        return x_shuffle
+
+
+class PhaseRemove(nn.Module):
+    def __init__(self):
+        super(PhaseRemove, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+@SerializableModule.register_model('discriminator')
+class Discriminator(SerializableModule):
+    def __init__(self, model_size=64, num_channels=1, shift_factor=2,
+                 alpha=0.2, verbose=False):
+        super().__init__()
+
+        self.model_size = model_size  # d
+        self.num_channels = num_channels  # c
+        self.shift_factor = shift_factor  # n
+        self.alpha = alpha
+        self.verbose = verbose
+
+        self.conv1 = nn.Conv1d(num_channels, model_size, 25, stride=4, padding=11)
+        self.conv2 = nn.Conv1d(model_size, 2 * model_size, 25, stride=4, padding=11)
+        self.conv3 = nn.Conv1d(2 * model_size, 4 * model_size, 25, stride=4, padding=11)
+        self.conv4 = nn.Conv1d(4 * model_size, 8 * model_size, 25, stride=4, padding=11)
+        self.conv5 = nn.Conv1d(8 * model_size, 16 * model_size, 25, stride=4, padding=11)
+
+        self.ps1 = PhaseShuffle(shift_factor)
+        self.ps2 = PhaseShuffle(shift_factor)
+        self.ps3 = PhaseShuffle(shift_factor)
+        self.ps4 = PhaseShuffle(shift_factor)
+
+        self.fc1 = nn.Linear(256 * model_size, 1)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
+                nn.init.kaiming_normal(m.weight.data)
+
+    def forward(self, x):
+        x = torch.nn.functional.leaky_relu(self.conv1(x), negative_slope=self.alpha)
+        if self.verbose:
+            print(x.shape)
+        x = self.ps1(x)
+
+        x = torch.nn.functional.leaky_relu(self.conv2(x), negative_slope=self.alpha)
+        if self.verbose:
+            print(x.shape)
+        x = self.ps2(x)
+
+        x = torch.nn.functional.leaky_relu(self.conv3(x), negative_slope=self.alpha)
+        if self.verbose:
+            print(x.shape)
+        x = self.ps3(x)
+
+        x = torch.nn.functional.leaky_relu(self.conv4(x), negative_slope=self.alpha)
+        if self.verbose:
+            print(x.shape)
+        x = self.ps4(x)
+
+        x = torch.nn.functional.leaky_relu(self.conv5(x), negative_slope=self.alpha)
+        if self.verbose:
+            print(x.shape)
+
+        x = x.view(-1, 256 * self.model_size)
+        if self.verbose:
+            print(x.shape)
+
+        return self.fc1(x)
